2b1cdcf878d840f90d5790f4764a6a2f125bfe8d
Antonio Ortega <ntoniohu@gmail.com>
2021-06-05 19:02:31 +0200

    Refactor and test merge module

--- R/merge.R
+++ R/merge.R
@@ -4,47 +4,57 @@
 #' represented by x and y, in such a way
 #' that x gains all the columns in y not present in x
 #' The merge is performed using the t column
+#' Both tables should refer to the same individual
+#' since this function is not id-aware
 #' @export
 merge_behavr <- function(x, y) {
   columns_x <- colnames(x)
   stopifnot("t" %in% colnames(x))
   stopifnot("t" %in% colnames(y))
+
+  # merge the data
   data <- merge(x, y[, c("t", setdiff(colnames(y), columns_x)), with = F], by = "t")
+  data <- data[, c("id", "t", setdiff(colnames(data), c("id", "t"))), with=F]
+
+  # merge the metadata
   meta_x <- x[, meta = T]
   meta_y <- y[, meta = T]
   metadata <- data.table::as.data.table(dplyr::full_join(meta_x, meta_y))
+
+  # set keys
   setkey(data, id)
   setkey(metadata, id)
 
-  setmeta(data, metadata)
+  # set the right order
 
+  # restore the behavr table
+  setmeta(data, metadata)
   return(data)
 }
 
-
-
 #' @export
+#' @rdname merge_behavr
+#' @details merge_behavr_all with multi individual tables
 merge_behavr_all <- function(x, y) {
 
-  data <- purrr::map(x[, unique(id)], function(i) {
-    merged <- merge_behavr(x[id == i], y[id == i])
+  x_id_column <- data.table::key(x)
+  y_id_column <- data.table::key(y)
+  x$id_ <- x[, x_id_column, with=F]
+  y$id_ <- y[, y_id_column, with=F]
 
-    metadata_x <- x[, meta = T][id == i]
-    metadata_y <- y[, meta = T][id == i]
-    metadata <- merge(
-      metadata_x,
-      metadata_y[, c("id", setdiff(colnames(metadata_y), colnames(metadata_x))), with = F],
-      by = "id"
-    )
-    if (!"id" %in% colnames(merged)) {
-      merged$id <- i
-    }
-    setkey(merged, id)
-    setmeta(merged, metadata)
+  unique_ids <- unique(x$id_[x$id_ %in% y$id_])
+
+  data <- purrr::map(unique_ids, function(id_value) {
+     res <- merge_behavr(
+       x[id_ == id_value, ],
+       y[id_ == id_value, ]
+     )
+     res[, id_ := NULL]
+
   })
 
-  bind_behavr_list(data)
-
+  merged <- bind_behavr_list(data)
+  return(merged)
 }
 
 #' Convenience wrapper around bin_apply_all and merge_behavr_all
@@ -53,37 +63,32 @@ merge_behavr_all <- function(x, y) {
 #' using function `summary_FUN`.
 #'
 #' @importFrom purrr map
+#' @param data A behavr table
+#' @param x Name of column to make bins of, normally "t" for time
+#' @param y Columns in data to compute aggregates for. Can be more than 1!
 #' @export
-bin_all <- function(data, x="t", y, x_bin_length = 30*60, FUN = mean, keep_columns=NULL) {
+bin_all <- function(data, x="t", y) {
 
-
-  # TODO Figure out how to do it with ...
   data <- purrr::map(
     y,
     ~bin_apply_all(
       data = data,
       y = .,
       x = x,
-      x_bin_length = x_bin_length,
-      FUN = FUN,
-      keep_columns = keep_columns
+      ...
     )
   )
 
-  sapply(data, function(x) "t" %in% colnames(x))
-  # if more than one variable was passed,
-  # merge the results
-  # all together
+  # if more than one variable was passed, merge the results
   if (all(sapply(data, function(x) inherits(x, "behavr"))))
     data <- Reduce(x = data, f = merge_behavr_all)
   else if (any(sapply(data, function(x) inherits(x, "behavr"))))
     stop("Some entries are of class behavr and some others are not")
-  else if (length(data) > 1)
-    data <- do.call(dplyr::full_join, data)
-  else
-    data <- data[[1]]
+  # else if (length(data) > 1)
+  #   data <- do.call(dplyr::full_join, data)
+  # else
+  #   data <- data[[1]]
 
   return(data)
 }
 
-# fslbehavr::bin_apply(laptop_test.csv, "asleep", x_bin_length = 2419200, FUN = mean)
--- tests/testthat/test-merge.R
+++ tests/testthat/test-merge.R
@@ -1,18 +1,28 @@
 test_that("merge_behavr works", {
-  original <- toy_activity_data(data.frame(id = 1), duration = days(1))
-  x <- copy(original)[, c("id", "t", "asleep")]
-  y <- copy(original)[, c("id", "t", "moving")]
+  original <- toy_activity_data(data.frame(id = 1), duration = days(1))[1:10,]
+  x <- copy(original)[, c("id", "t", "moving")]
+  y <- copy(original)[, c("id", "t", "asleep")]
   derived <- merge_behavr(x , y)
-  expect_true(all(derived[, colnames(original), with = F] == original))
+  expect_true(identical(
+    derived,
+    original
+  ))
 
 })
 
 
 test_that("merge_behavr_all works", {
-  original <- toy_activity_data(data.frame(id = 1:3), duration = days(1))
-  x <- copy(original)[, c("id", "t", "asleep")]
-  y <- copy(original)[, c("id", "t", "moving")]
+  original <- toy_activity_data(data.frame(id = 1:3), duration = days(1))[1:20]
+  metadata <- data.table(id = 1:2)
+
+  original[11:20, id:=2]
+  data.table::setkey(original, id)
+  data.table::setkey(metadata, id)
+  setmeta(original, metadata)
+
+  x <- copy(original)[, c("id", "t", "moving")]
+  y <- copy(original)[, c("id", "t", "asleep")]
   derived <- merge_behavr_all(x, y)
 
-  expect_true(all(derived[, colnames(original), with = F] == original))
+  expect_true(identical(derived, original))
 })
