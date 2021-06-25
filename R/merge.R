36dc404a788f171d34f11287e791792e1ed3546c
Antonio Ortega <ntoniohu@gmail.com>
2021-06-25 03:37:42 +0200

    Patch for multikey behavrs in merge_behavr_all. Not tested with testthat but seems to do the job when tested manually. I need to write the test

--- R/merge.R
+++ R/merge.R
@@ -10,8 +10,7 @@
 #' @param y second behavr table to merge
 #' @importFrom dplyr full_join
 #' @export
-merge_behavr <- function(x, y) {
-
+merge_behavr <- function(x, y, merge_meta=TRUE) {
   id <- NULL
   columns_x <- colnames(x)
   stopifnot("t" %in% colnames(x))
@@ -21,25 +20,26 @@ merge_behavr <- function(x, y) {
   data <- merge(x, y[, c("t", setdiff(colnames(y), columns_x)), with = F], by = "t")
   data <- data[, c("id", "t", setdiff(colnames(data), c("id", "t"))), with=F]
 
-  # merge the metadata
-  meta_x <- x[, meta = T]
-  meta_y <- y[, meta = T]
-  metadata <- data.table::as.data.table(dplyr::full_join(meta_x, meta_y))
+  if (merge_meta) {
+    # merge the metadata
+    meta_x <- x[, meta = T]
+    meta_y <- y[, meta = T]
+    metadata <- data.table::as.data.table(dplyr::full_join(meta_x, meta_y))
 
-  # set keys
-  setkey(data, id)
-  setkey(metadata, id)
+    # set keys
+    setkey(data, id)
+    setkey(metadata, id)
 
-  # set the right order
+    # set the right order
 
-  # restore the behavr table
-  setmeta(data, metadata)
+    # restore the behavr table
+    setmeta(data, metadata)
+  }
   return(data)
 }
 
 #' @rdname merge_behavr
 #' @details merge_behavr with multi individual tables
-#' @importFrom purrr map
 #' @export
 merge_behavr_all <- function(x, y) {
 
@@ -47,21 +47,31 @@ merge_behavr_all <- function(x, y) {
 
   x_id_column <- data.table::key(x)
   y_id_column <- data.table::key(y)
-  x$id_ <- x[, x_id_column, with=F]
-  y$id_ <- y[, y_id_column, with=F]
+  x$id_ <- apply(MARGIN=1, X=x[, x_id_column, with=F], FUN = function(x) paste(x, collapse="-"))
+  y$id_ <- apply(MARGIN=1, X=y[, y_id_column, with=F], FUN = function(x) paste(x, collapse="-"))
 
   unique_ids <- unique(x$id_[x$id_ %in% y$id_])
 
-  data <- purrr::map(unique_ids, function(id_value) {
+  data <- lapply(unique_ids, function(id_value) {
      res <- merge_behavr(
        x[id_ == id_value, ],
-       y[id_ == id_value, ]
+       y[id_ == id_value, ],
+       merge_meta = FALSE
      )
      res[, id_ := NULL]
 
   })
 
-  merged <- bind_behavr_list(data)
+  if("behavr" %in% class(data[[1]])) {
+    merged <- bind_behavr_list(data)
+  } else {
+    # the behavrs break when x and y have multikeys
+    # but this restores the behavr objects
+    merged <- do.call(rbind, data)
+    setkey(merged, id)
+    setmeta(merged, behavr::meta(x))
+  }
+
   return(merged)
 }
 
